a2426b0bc2d22ce63870c84c0c408a84a66dab1e
datasha1 <sandro.datashvili.1@btu.edu.ge>
2020-12-11 23:48:16 +0400

    Homework of mobile app

---
+++ app/src/main/java/com/example/myapplicationFragments/Notification.kt
@@ -0,0 +1,22 @@
+package com.example.myapplicationFragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class Notification : Fragment(R.layout.fragment_notification) {
+
+    private lateinit var textView: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        textView = view.findViewById(R.id.textView2)
+
+        textView.text = NotificationArgs.fromBundle(requireArguments()).amount.toString()
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplicationFragments/message.kt
@@ -0,0 +1,7 @@
+package com.example.myapplicationFragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class message : Fragment(R.layout.fragment_message) {
+}
